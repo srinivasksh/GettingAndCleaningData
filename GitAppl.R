77226b45f019beea51866a61c101706aab6e7612
srinivas.ksh <srinivas.ksheerasagara@gmail.com>
2015-12-20 12:10:51 +0530

    added GitAppl.R

---
+++ GitAppl.R
@@ -0,0 +1,19 @@
+library(httr)
+
+# 1. Find OAuth settings for github:
+#    http://developer.github.com/v3/oauth/
+oauth_endpoints("github")
+
+# authenticate app using key and secret
+myapp <- oauth_app("github",
+                   key = "9cfba4c3f7a578de81ca",
+                   secret = "62c434fe2ef946cbb662f56477a3ba167b41343e")
+
+# 3. Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# 4. Use API to extract content from mentioned repo
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+stop_for_status(req)
+content(req)
